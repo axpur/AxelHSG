fc252175dc786be10c10a022e98676c5c00d7d12
tadas.gedminas <gedmintadas@gmail.com>
2017-12-01 12:59:21 +0100

    Adjusted server file

---
+++ server.py
@@ -0,0 +1,79 @@
+from mesa.visualization.ModularVisualization import ModularServer
+from mesa.visualization.modules import CanvasGrid, ChartModule, TextElement
+from mesa.visualization.UserParam import UserSettableParameter
+
+from mesa.visualization.TextVisualization import (
+    TextData, TextGrid, TextVisualization
+)
+
+from model2 import SchellingModel_vote
+
+
+class SchellingTextVisualization(TextVisualization):
+    '''
+    ASCII visualization for schelling model
+    '''
+
+    def __init__(self, model):
+        '''
+        Create new Schelling ASCII visualization.
+        '''
+        self.model = model
+
+        grid_viz = TextGrid(self.model.grid, self.ascii_agent)
+        happy_viz = TextData(self.model, 'happy')
+        self.elements = [grid_viz, happy_viz]
+
+    @staticmethod
+    def ascii_agent(a):
+        '''
+        Minority agents are X, Majority are O.
+        '''
+        if a.type == 0:
+            return 'O'
+        if a.type == 1:
+            return 'X'
+
+
+class HappyElement(TextElement):
+    '''
+    Display a text count of how many happy agents there are.
+    '''
+    def __init__(self):
+        pass
+
+    def render(self, model):
+        return "Happy agents: " + str(model.happy)
+
+
+def schelling_draw(agent):
+    '''
+    Portrayal Method for canvas
+    '''
+    if agent is None:
+        return
+    portrayal = {"Shape": "circle", "r": 0.5, "Filled": "true", "Layer": 0}
+
+    if agent.type == 0:
+        portrayal["Color"] = "Red"
+    else:
+        portrayal["Color"] = "Blue"
+    return portrayal
+
+
+happy_element = HappyElement()
+canvas_element = CanvasGrid(schelling_draw, 33, 33, 500, 500)
+happy_chart = ChartModule([{"Label": "happy", "Color": "Black"}])
+
+model_params = {
+    "height": 33,
+    "width": 33,
+    "density": UserSettableParameter("slider", "Agent density", 0.7, 0.1, 1.0, 0.1),
+    "minority_pc": UserSettableParameter("slider", "Fraction minority", 0.5, 0.00, 1.0, 0.05),
+    "homophily": UserSettableParameter("slider", "Homophily", 5, 0, 8, 1)
+}
+
+server = ModularServer(SchellingModel_vote,
+                       [canvas_element, happy_element, happy_chart],
+                       "Schelling", model_params)
+server.launch()
