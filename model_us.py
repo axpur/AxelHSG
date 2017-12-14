f3058a93fcc9b89affb84901271ae7abc59e6da3
tadas.gedminas <gedmintadas@gmail.com>
2017-12-14 16:54:56 +0100

    New US files

--- model_us.py
+++ model_us.py
@@ -10,7 +10,7 @@ class SchellingAgent(Agent):
     '''
     Schelling segregation agent
     '''
-    # Q: What is the purpose of _init_ exactly
+    # Q: What is the purpose of _init_ exactly see
     def __init__(self, pos, model, agent_type):
         '''
          Create a new Schelling agent.
@@ -22,6 +22,9 @@ class SchellingAgent(Agent):
         super().__init__(pos, model)
         self.pos = pos
         self.type = agent_type
+        self.utility = 0
+        self.elections_utility = 0
+        self.neighbor_types = []
         # Determine to which location you belong in terms of X
         if self.pos[0] >= 23:
             self.x = 2
@@ -43,20 +46,37 @@ class SchellingAgent(Agent):
 
         # Tracking the number of different types of agents in location
         if self.type == 0:
-            self.model.elections_type0[self.loc] += 1
+            self.model.elections_party0[self.loc] += 1
+        elif self.type == 2:
+            self.model.elections_center_0[self.loc] += 1 #left wing of the center party
+        elif self.type == 3:
+            self.model.elections_center_1[self.loc] += 1 #right wing of the center party
         else:
-            self.model.elections_type1[self.loc] += 1
+            self.model.elections_party1[self.loc] += 1
 
     def step(self):
-
+        self.neighbor_types = []
         similar = 0  # How many agents around me are similar to me. Initially -1. Done for each agent at every step.
+        self.election_utility = 0
         for neighbor in self.model.grid.neighbor_iter(self.pos):
+            self.neighbor_types.append(neighbor.type)
             if neighbor.type == self.type:
                 similar += 1
+            if (self.type == 1 and neighbor.type == 2) or \
+               (self.type == 2 and neighbor.type == 1) or \
+               (self.type == 0 and neighbor.type == 3) or \
+               (self.type == 3 and neighbor.type == 0):
+                similar += self.model.alpha
 
         # Check whether your type matches the type that won the election in your location
-        if self.type == self.model.elections[self.loc]:
+        if (self.type == 1 and self.model.elections[self.loc] == 1) or \
+           (self.type == 2 and self.model.elections[self.loc] == 1) or \
+           (self.type == 0 and self.model.elections[self.loc] == 0) or \
+           (self.type == 3 and self.model.elections[self.loc] == 0):
             similar = similar + 1
+            self.election_utility += 1
+
+        self.utility = similar
         # If unhappy, move:
         if similar < self.model.homophily:
             # Simplifies location adjustment
@@ -86,9 +106,13 @@ class SchellingAgent(Agent):
 
         # Tracking the number of different types of agents in location
         if self.type == 0:
-            self.model.elections_type0[self.loc] += 1
+            self.model.elections_party0[self.loc] += 1
+        elif self.type == 2:
+            self.model.elections_center_0[self.loc] += 1
+        elif self.type == 3:
+            self.model.elections_center_1[self.loc] += 1
         else:
-            self.model.elections_type1[self.loc] += 1
+            self.model.elections_party1[self.loc] += 1
 
 
 class SchellingModel_US(Model):
@@ -96,16 +120,18 @@ class SchellingModel_US(Model):
     Model class for the Schelling segregation model.
     '''
 
-    def __init__(self, height, width, density, minority_pc, homophily):
+    def __init__(self, height, width, density, type_1, type_2, type_3, homophily, alpha):
         '''
         '''
         # Setting up the Model
         self.height = height
         self.width = width
         self.density = density  # percentage (empty houses)
-        self.minority_pc = minority_pc  # percentage minority in the city
+        self.type_1 = type_1  # percentage minority in the city
+        self.type_2 = type_2
+        self.type_3 = type_3
         self.homophily = homophily  # number of similar minded person that you want around you
-
+        self.alpha = alpha
         # Setting up the AGM simulation
         self.schedule = RandomActivation(self)
 
@@ -121,10 +147,15 @@ class SchellingModel_US(Model):
         # Setting a variable to store total number of different types of agents
         self.type0 = 0
         self.type1 = 0
+        self.type2 = 0
+        self.type3 = 0
 
         # Setting up lists to store location specific values
-        self.elections_type0 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-        self.elections_type1 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.elections_party0 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.elections_party1 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.elections_center = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.elections_center_0 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.elections_center_1 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
         self.elections_type_total = [0, 0, 0, 0, 0, 0, 0, 0, 0]
         self.elections = [0, 0, 0, 0, 0, 0, 0, 0, 0]
         # Set up agents
@@ -139,16 +170,24 @@ class SchellingModel_US(Model):
             # First if statement: take a random number between 0 and 1
             # (random.random command) and check whether that value is
             # below the assigned density.
-
+            random_number = random.random()
             # Second if statement: take a random number between 0 and 1
             # and assign the agent type based on the condition
             if random.random() < self.density:
-                if random.random() < self.minority_pc:
+                if random_number < self.type_1:
                     agent_type = 1
                     self.type1 += 1
                 else:
-                    agent_type = 0
-                    self.type0 += 1
+                    if random_number < (self.type_1+self.type_2):
+                        agent_type = 2
+                        self.type2 += 1 # E: the variable tracks total number of different types, so should be adding one
+                    else:
+                        if random_number < (self.type_1+self.type_2+self.type_3):
+                            agent_type = 3
+                            self.type3 += 1 # E: Same as last
+                        else:
+                            agent_type = 0
+                            self.type0 += 1 # E:Missing plus sign
 
                 # Refer to the above function related to Agent attributes
                 agent = SchellingAgent((x, y), self, agent_type)
@@ -157,11 +196,9 @@ class SchellingModel_US(Model):
 
         # For each location run the elections (range(9) goes from 0 to 8)
         for i in range(9):
-            # Total number of agents in location i
-            self.elections_type_total[i] = self.elections_type0[i] + self.elections_type1[i]
-
-            # Election winner criteria
-            if self.elections_type1[i] >= self.elections_type0[i]:
+            #add center left and center right together
+            self.elections_type_total[i] = self.elections_party0[i] + self.elections_party1[i] + self.elections_center_1[i] + self.elections_center_0[i]
+            if self.elections_party1[i] + self.elections_center_1[i] >= self.elections_party0[i] + self.elections_center_0[i]:
                 self.elections[i] += 1
 
         # Storing relevant data for calculating segregation measures
@@ -169,10 +206,18 @@ class SchellingModel_US(Model):
             {"happy": lambda m: m.happy,
             "total_0": lambda m: m.type0,
             "total_1": lambda m: m.type1,
-            "location_0": lambda m: m.elections_type0,
-            "location_1": lambda m: m.elections_type1,
+            "total_2": lambda m: m.type2,
+            "total_3": lambda m: m.type3,
+            "location_0": lambda m: m.elections_party0,
+            "location_1": lambda m: m.elections_party1,
+            "location_2": lambda m: m.elections_center_0,
+            "location_3": lambda m: m.elections_center_1,
             "location_total": lambda m: m.elections_type_total,
-            "elections": lambda m: m.elections})
+            "elections": lambda m: m.elections},
+            {"x": lambda a: a.pos[0], "y": lambda a: a.pos[1],
+            "util": lambda a: a.utility, "loc": lambda a: a.loc,
+            "type": lambda a: a.type, "neighbor_types": lambda a: a.neighbor_types,
+            "elec_util": lambda a: a.election_utility})
 
     def step(self):
         '''
@@ -180,8 +225,10 @@ class SchellingModel_US(Model):
         '''
 
         # Reseting location specific lists
-        self.elections_type0 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-        self.elections_type1 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.elections_party0 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.elections_party1 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.elections_center_0 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.elections_center_1 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
         self.elections_type_total = [0, 0, 0, 0, 0, 0, 0, 0, 0]
         self.happy = 0  # Reset counter of happy agents
         self.schedule.step()
@@ -190,8 +237,9 @@ class SchellingModel_US(Model):
 
         # Re-running elections after agents have moved
         for i in range(9):
-            self.elections_type_total[i] = self.elections_type0[i] + self.elections_type1[i]
-            if self.elections_type1[i] >= self.elections_type0[i]:
+            #add center left and center right together
+            self.elections_type_total[i] = self.elections_party0[i] + self.elections_party1[i] + self.elections_center_1[i] + self.elections_center_0[i]
+            if self.elections_party1[i] + self.elections_center_1[i] >= self.elections_party0[i] + self.elections_center_0[i]:
                 self.elections[i] += 1
 
         # Storing relevant data
--- server_us.py
+++ server_us.py
@@ -6,6 +6,7 @@ from mesa.visualization.TextVisualization import (
     TextData, TextGrid, TextVisualization
 )
 
+#from model import SchellingModel
 from model_us import SchellingModel_US
 
 
@@ -33,6 +34,10 @@ class SchellingTextVisualization(TextVisualization):
             return 'O'
         if a.type == 1:
             return 'X'
+        if a.type == 2:
+            return 'L'
+        if a.type == 3:
+            return 'K'
 
 
 class HappyElement(TextElement):
@@ -45,7 +50,6 @@ class HappyElement(TextElement):
     def render(self, model):
         return "Happy agents: " + str(model.happy)
 
-
 def schelling_draw(agent):
     '''
     Portrayal Method for canvas
@@ -54,12 +58,19 @@ def schelling_draw(agent):
         return
     portrayal = {"Shape": "circle", "r": 0.5, "Filled": "true", "Layer": 0}
 
-    if agent.type == 0:
+    if agent.type == 1:
         portrayal["Color"] = "Red"
-    else:
-        portrayal["Color"] = "Blue"
-    return portrayal
 
+    if agent.type == 2:
+        portrayal["Color"] = "Pink"
+
+    if agent.type ==3:
+        portrayal["Color"] = "Lightblue"
+
+    if agent.type == 0:
+        portrayal["Color"] = "Blue"
+
+    return portrayal
 
 happy_element = HappyElement()
 canvas_element = CanvasGrid(schelling_draw, 33, 33, 500, 500)
@@ -69,8 +80,11 @@ model_params = {
     "height": 33,
     "width": 33,
     "density": UserSettableParameter("slider", "Agent density", 0.7, 0.1, 1.0, 0.1),
-    "minority_pc": UserSettableParameter("slider", "Fraction minority", 0.5, 0.00, 1.0, 0.05),
-    "homophily": UserSettableParameter("slider", "Homophily", 5, 0, 8, 1)
+    "type_1": UserSettableParameter("slider", "Fraction type 1", 0.33, 0.00, 1, 0.05),
+    "type_2": UserSettableParameter("slider", "Fraction type 2", 0.16, 0.00, 1, 0.05),
+    "type_3": UserSettableParameter("slider", "Fraction type 3", 0.17, 0.00, 1, 0.05),
+    "homophily": UserSettableParameter("slider", "Homophily", 3, 0, 8, 1),
+    "alpha": UserSettableParameter("slider", "Alpha", 1, 0, 0.5, 0.1)
 }
 
 server = ModularServer(SchellingModel_US,
