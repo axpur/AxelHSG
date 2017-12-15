d54ad4fd6808377dd384c9d912720ecf8270a128
tadas.gedminas <gedmintadas@gmail.com>
2017-12-15 11:12:08 +0100

    Example case of running the simulation and analysing the data

--- adj_output.R
+++ adj_output.R
@@ -1,11 +1,11 @@
 #### Setting workng directory ####
-setwd("C:\\Users\\gedmi\\Desktop\\HSG\\Python practice\\Schelling with mesa")
+setwd("C:\\Users\\gedmi\\Desktop\\SE Project\\Further\\New\\AxelHSG")
 
 #### Libraries ####
 library(tidyverse)
 
 #### Adjusting Python data ####
-data_out <- read.csv("out.csv", stringsAsFactors = F)
+data_out <- read.csv("data/out_us.csv", stringsAsFactors = F)
 
 ## Remove brackets from Python list data
 data_out$elections <- gsub(pattern = "\\[", replacement = "", x = data_out$elections)
@@ -17,80 +17,60 @@ data_out$location_0 <- gsub(pattern = "\\]", replacement = "", x = data_out$loca
 data_out$location_1 <- gsub(pattern = "\\[", replacement = "", x = data_out$location_1)
 data_out$location_1 <- gsub(pattern = "\\]", replacement = "", x = data_out$location_1)
 
+data_out$location_2 <- gsub(pattern = "\\[", replacement = "", x = data_out$location_2)
+data_out$location_2 <- gsub(pattern = "\\]", replacement = "", x = data_out$location_2)
+
+data_out$location_3 <- gsub(pattern = "\\[", replacement = "", x = data_out$location_3)
+data_out$location_3 <- gsub(pattern = "\\]", replacement = "", x = data_out$location_3)
+
 data_out$location_total <- gsub(pattern = "\\[", replacement = "", x = data_out$location_total)
 data_out$location_total <- gsub(pattern = "\\]", replacement = "", x = data_out$location_total)
 
 ## Renaming ID column
-colnames(data_out)[1] <- "run"
-# 
-# ## Generate column names for list data
-# elections_cols <- c()
-# loc0_cols <- c()
-# loc1_cols <- c()
-# loc_total_cols <- c()
-# 
-# for (i in 1:9){
-#   elections_cols[i] <- paste0("elec", i)
-#   loc0_cols[i] <- paste0("loc0_", i)
-#   loc1_cols[i] <- paste0("loc1_", i)
-#   loc_total_cols[i] <- paste0("loc_total_", i)
-# }
-# 
-# 
-# ## Create empty dataframe to store adjusted dat
-# data_values <- as.data.frame(matrix(NA, nrow = nrow(data_out), ncol = 4 + length(elections_cols) * 4))
-# 
-# # Rename the dataframe
-# colnames(data_values) <- c("run", "happy", "total_0", "total_1", elections_cols, loc0_cols, loc1_cols, loc_total_cols)
-# 
-# ## Assigning non-list data
-# data_values$happy <- data_out$happy
-# data_values$run <- data_out$run
-# data_values$total_0 <- data_out$total_0
-# data_values$total_1 <- data_out$total_1
-# 
-# ## Looping over each list type data to R workable method
-# for (j in 1:nrow(data_out)){
-#   ## 'strsplit' - since Python list data is read as a string, strsplit splits the string 
-#   # at each comma in this case. Then as.numeric transforms into a numeric vector
-#   data_values[j, elections_cols] <- as.numeric(strsplit(data_out[j, "elections"], ",")[[1]])
-#   data_values[j, loc0_cols] <- as.numeric(strsplit(data_out[j, "location_0"], ",")[[1]])
-#   data_values[j, loc1_cols] <- as.numeric(strsplit(data_out[j, "location_1"], ",")[[1]])
-#   data_values[j, loc_total_cols] <- as.numeric(strsplit(data_out[j, "location_total"], ",")[[1]])
-# }
+colnames(data_out)[1] <- "step"
 
 ## Alternative cleaning
 # Defining list of parameters of the run
 params <- list()
-params$runs <- length(data_out$run)
-params$types <- 2
+params$steps <- length(unique(data_out$step))
+params$types <- 4
 params$locations <- 9
+params$runs <- 10
 
 # Creating an empty dataframe for storing values
-data_store <- as.data.frame(matrix(NA, nrow = params$runs * params$types * params$locations, ncol = 8))
-colnames(data_store) <- c("run", "happy", "loc", "elect", "type", "group_total", "loc_grp_total", "loc_total")
+data_store <- as.data.frame(matrix(NA, nrow = params$runs * params$types * params$locations * params$steps, ncol = 9))
+colnames(data_store) <- c("run", "steps", "happy", "loc", "elect", "type", "group_total", "loc_grp_total", "loc_total")
 
 # Defining case identifiers
 data_store$run <- rep(data_out$run, each = params$types * params$locations)
 data_store$happy <- rep(data_out$happy, each = params$types * params$locations)
-data_store$loc <- rep(1:params$locations, times = params$runs * params$types)
-data_store$type <- rep(1:params$types, times = params$runs, each = params$locations)
+data_store$loc <- rep(1:params$locations, times = params$runs * params$types * params$runs)
+data_store$type <- rep(1:params$types, times = params$runs*params$steps, each = params$locations)
+data_store$steps <- rep(data_out$step, each = params$types * params$locations)
 
 # Matching identifiers with data
-for (z in 1:nrow(data_out)) {
-  data_store[data_store$run == data_out[z, "run"], "elect"] <- rep(as.numeric(strsplit(data_out[z, "elections"], ",")[[1]]), 
-                                                                   times = params$types)
-  data_store[data_store$run == data_out[z, "run"] & data_store$type == 1, "loc_grp_total"] <- as.numeric(strsplit(data_out[z, "location_0"], ",")[[1]])
-  data_store[data_store$run == data_out[z, "run"] & data_store$type == 2, "loc_grp_total"] <- as.numeric(strsplit(data_out[z, "location_1"], ",")[[1]])
-  data_store[data_store$run == data_out[z, "run"] & data_store$type == 1, "group_total"] <- data_out[z, "total_0"]
-  data_store[data_store$run == data_out[z, "run"] & data_store$type == 2, "group_total"] <- data_out[z, "total_1"]
-  data_store[data_store$run == data_out[z, "run"], "loc_total"] <- rep(as.numeric(strsplit(data_out[z, "location_total"], ",")[[1]]), 
+for (x in 1:length(unique(data_out$run))){
+  data_out_loop <- filter(data_out, run == x)
+    for (z in 1:length(unique(data_out_loop$step))) {
+      data_store[data_store$steps == data_out[z, "step"] & data_store$run == x, "elect"] <- rep(as.numeric(strsplit(data_out_loop[z, "elections"], ",")[[1]]), 
                                                                        times = params$types)
+      
+      data_store[data_store$steps == data_out[z, "step"] & data_store$run == x & data_store$type == 1, "loc_grp_total"] <- as.numeric(strsplit(data_out_loop[z, "location_0"], ",")[[1]])
+      data_store[data_store$steps == data_out[z, "step"] & data_store$run == x & data_store$type == 2, "loc_grp_total"] <- as.numeric(strsplit(data_out_loop[z, "location_1"], ",")[[1]])
+      data_store[data_store$steps == data_out[z, "step"] & data_store$run == x & data_store$type == 3, "loc_grp_total"] <- as.numeric(strsplit(data_out_loop[z, "location_2"], ",")[[1]])
+      data_store[data_store$steps == data_out[z, "step"] & data_store$run == x & data_store$type == 4, "loc_grp_total"] <- as.numeric(strsplit(data_out_loop[z, "location_3"], ",")[[1]])
+      
+      data_store[data_store$steps == data_out[z, "step"] & data_store$run == x & data_store$type == 1, "group_total"] <- data_out_loop[z, "total_0"]
+      data_store[data_store$steps == data_out[z, "step"] & data_store$run == x & data_store$type == 2, "group_total"] <- data_out_loop[z, "total_1"]
+      data_store[data_store$steps == data_out[z, "step"] & data_store$run == x & data_store$type == 3, "group_total"] <- data_out_loop[z, "total_2"]
+      data_store[data_store$steps == data_out[z, "step"] & data_store$run == x & data_store$type == 4, "group_total"] <- data_out_loop[z, "total_3"]
+      data_store[data_store$steps == data_out[z, "step"] & data_store$run == x, "loc_total"] <- rep(as.numeric(strsplit(data_out_loop[z, "location_total"], ",")[[1]]), 
+                                                                           times = params$types)
+    }
 }
-
 # Calculating intermediate values for calculating measures
 data_calc <- data_store %>%
-  group_by(run, loc) %>%
+  group_by(run, steps, loc) %>%
   mutate(total = sum(group_total),
          pi_m = group_total/total,
          pi_jm = loc_grp_total/loc_total,
@@ -102,19 +82,19 @@ data_calc <- data_store %>%
 
 # Calculating seggregation measures. Names correspond to the numbering in Notebook files
 data_ratios <- data_calc %>%
-  group_by(run) %>%
+  group_by(run, steps) %>%
   summarize(one = 0.5*sum((loc_total/(total*i))*(abs(pi_jm - pi_m))),
             two = sum((loc_total/(total*e))*pi_jm*log(pi_jm_log/pi_m)),
             four = sum((loc_total/total)* ((pi_jm - pi_m)^2)/((params$types - 1)*pi_m)),
             five = sum((loc_total/total)*(pi_jm-pi_m)^2))
 
 data_ratios2 <- data_calc %>%
-  group_by(run, type) %>%
+  group_by(run, type, steps) %>%
   summarize(three = sum(pi_j * pi_jm))
 
 # Segregation measures over runs in the simulation
-ratio_plotter <- ggplot(gather(data_ratios, key = "Ratio", value = "Value", one:five), aes(x = run, y = Value, color = Ratio)) + 
-  geom_line(size = 1) + theme_bw() + theme(legend.position = "bottom") + ggtitle("Ratios over simulation")
+ratio_plotter <- ggplot(gather(data_ratios, key = "Ratio", value = "Value", one:five), aes(x = steps, y = Value, color = Ratio)) + 
+  geom_line(size = 1) + theme_bw() + theme(legend.position = "bottom") + ggtitle("Ratios over simulation") + facet_wrap(~run)
 
 ratio_plotter
 
--- data_collect.py
+++ data_collect.py
@@ -1,11 +1,28 @@
-from model_us import SchellingModel_US
+from US_elections.model_us import SchellingModel_US
+from UK_elections.model_uk import SchellingModel_UK
+from AUS_elections.model_aus import SchellingModel_AUS
 import random
+import pandas as pd
+import numpy as np
 
 random.seed(1234)
-model = SchellingModel_US(33, 33, 0.7, 0.5, 5)
+# Defining all models
+# model_us = SchellingModel_US(33, 33, 0.7, 0.33, 0.16, 0.17, 5, 1, 0.5)
+# model_uk = SchellingModel_UK(33, 33, 0.7, 0.33, 0.16, 0.17, 5, 1, 0.5)
+# model_aus = SchellingModel_AUS(33, 33, 0.7, 0.33, 0.16, 0.17, 5, 1, 0.5)
 
-while model.running and model.schedule.steps < 100:
-    model.step()
+# Running multiple times and storing as a csv file
+for i in range(10):
+    model_us = SchellingModel_US(33, 33, 0.7, 0.33, 0.16, 0.17, 5, 1, 0.5)  # Re-defining the model
+    while model_us.running and model_us.schedule.steps < 100:
+        model_us.step()
+    model_us_data = model_us.datacollector.get_model_vars_dataframe()  # Storing data of a single run
+    ider = i+1  # Run identifer
+    print(ider)
+    model_us_data['run'] = np.repeat(ider, len(model_us_data))  # Adding run identifier
+    if i == 0:
+        model_us_all = model_us_data  # If it is first run assign to the dataframe
+    else:
+        model_us_all = model_us_all.append(model_us_data)  # Otherwise row bind
 
-model_out = model.datacollector.get_model_vars_dataframe()
-model_out.to_csv('out.csv')
+model_us_all.to_csv('data/out_us.csv')  # Save the file
