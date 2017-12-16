1c8aade77ccbb7f51665237a3a5e771c588856eb
tadas.gedminas <gedmintadas@gmail.com>
2017-12-16 10:36:02 +0100

    Added data collection for all country + comparison of different segregation measures across country cases

--- adj_output.R
+++ adj_output.R
@@ -22,23 +22,13 @@ cleaning_data <- function(params, data_out){
   data_out$location_total <- gsub(pattern = "\\[", replacement = "", x = data_out$location_total)
   data_out$location_total <- gsub(pattern = "\\]", replacement = "", x = data_out$location_total)
   
-  ## Renaming ID column
-  colnames(data_out)[1] <- "step"
-  
-  ## Alternative cleaning
-  # Defining list of parameters of the run
-  params <- list()
-  params$steps <- length(unique(data_out$step))
-  params$types <- 4
-  params$locations <- 9
-  params$runs <- 10
-  
   # Creating an empty dataframe for storing values
-  data_store <- as.data.frame(matrix(NA, nrow = params$runs * params$types * params$locations * params$steps, ncol = 10))
-  colnames(data_store) <- c("run", "steps", "happy", "seg_agents", "loc", "elect", "type", "group_total", "loc_grp_total", "loc_total")
+  data_store <- as.data.frame(matrix(NA, nrow = params$runs * params$types * params$locations * params$steps, ncol = 11))
+  colnames(data_store) <- c("cnt","run", "steps", "happy", "seg_agents", "loc", "elect", "type", "group_total", "loc_grp_total", "loc_total")
   
   # Defining case identifiers
   data_store$run <- rep(data_out$run, each = params$types * params$locations)
+  data_store$cnt <- data_out[1,"cnt"]
   data_store$happy <- rep(data_out$happy, each = params$types * params$locations)
   data_store$seg_agents <- rep(data_out$seg_agents, each = params$types * params$locations)
   data_store$loc <- rep(1:params$locations, times = params$runs * params$types * params$runs)
@@ -68,101 +58,164 @@ cleaning_data <- function(params, data_out){
   return(data_store)
 }
 
+agg_ratios <- function(data_store){
+  data_calc <- data_store %>%
+    group_by(run, steps, loc) %>%
+    mutate(total = sum(group_total),
+           pi_m = group_total/total,
+           pi_jm = loc_grp_total/loc_total,
+           pi_j = loc_grp_total/group_total,
+           i = sum(pi_m*(1-pi_m)),
+           e = sum(pi_m*log(1/pi_m)),
+           pi_jm_log = ifelse(pi_jm == 0, 0.001, pi_jm),
+           other_types_total = total-group_total,
+           other_types_loc = loc_total - loc_grp_total,
+           pi_other = other_types_total/total,
+           pi_other_j = other_types_loc/loc_total,
+           pi_other_j_log = ifelse(pi_other_j == 0, 0.001, pi_other_j),
+           e_grp = (pi_m*log(1/pi_m)) + (pi_other*log(1/pi_other)),
+           share_happy = happy/total,
+           share_seg = seg_agents/total) %>%
+    ungroup()
+  
+  # Calculating seggregation measures. Names correspond to the numbering in Notebook files
+  data_ratios <- data_calc %>%
+    group_by(run, steps, cnt) %>%
+    summarize(one = 0.5*sum((loc_total/(total*i))*(abs(pi_jm - pi_m))),
+              two = sum((loc_total/(total*e))*pi_jm*log(pi_jm_log/pi_m)),
+              four = sum((loc_total/total)* ((pi_jm - pi_m)^2)/((params$types - 1)*pi_m)),
+              five = sum((loc_total/total)*(pi_jm-pi_m)^2),
+              share_happy = mean(share_happy),
+              share_seg = mean(share_seg))
+  
+  return(data_ratios)
+}
+
+grp_ratios <- function(data_store){
+  # Calculating intermediate values for calculating measures
+  data_calc <- data_store %>%
+    group_by(run, steps, loc) %>%
+    mutate(total = sum(group_total),
+           pi_m = group_total/total,
+           pi_jm = loc_grp_total/loc_total,
+           pi_j = loc_grp_total/group_total,
+           i = sum(pi_m*(1-pi_m)),
+           e = sum(pi_m*log(1/pi_m)),
+           pi_jm_log = ifelse(pi_jm == 0, 0.001, pi_jm),
+           other_types_total = total-group_total,
+           other_types_loc = loc_total - loc_grp_total,
+           pi_other = other_types_total/total,
+           pi_other_j = other_types_loc/loc_total,
+           pi_other_j_log = ifelse(pi_other_j == 0, 0.001, pi_other_j),
+           e_grp = (pi_m*log(1/pi_m)) + (pi_other*log(1/pi_other)),
+           share_happy = happy/total,
+           share_seg = seg_agents/total) %>%
+    ungroup()
+  
+  data_ratios2 <- data_calc %>%
+    group_by(run, type, steps, cnt) %>%
+    summarize(three = sum(pi_j * pi_jm),
+              another_one = sum((loc_total/(total*e_grp))*pi_jm*log(pi_jm_log/pi_m) + (loc_total/(total*e_grp))*pi_other_j*log(pi_other_j_log/pi_other)))
+  
+  return(data_ratios2)
+}
+
 #### Libraries ####
 library(tidyverse)
 library(xtable)
 
+start_time <- Sys.time()
 #### Adjusting Python data ####
-data_out <- read.csv("data/out_us.csv", stringsAsFactors = F)
+data_us <- read.csv("data/out_us.csv", stringsAsFactors = F)
+colnames(data_us)[1] <- "step"
+
+data_uk <- read.csv("data/out_uk.csv", stringsAsFactors = F)
+colnames(data_uk)[1] <- "step"
+
+data_aus <- read.csv("data/out_aus.csv", stringsAsFactors = F)
+colnames(data_aus)[1] <- "step"
 
 # Defining parameters of the simulation
 params <- list()
-params$steps <- length(unique(data_out$step))
+params$steps <- length(unique(data_us$step))
 params$types <- 4
 params$locations <- 9
-params$runs <- 10
+params$runs <- 20
 
-# Cleaning data (needs to be tested with other country examples)
-data_store <- cleaning_data(params = params, data_out = data_out)
+# Cleaning data for each country
+us_store <- cleaning_data(params = params, data_out = data_us)
+uk_store <- cleaning_data(params = params, data_out = data_uk)
+aus_store <- cleaning_data(params = params, data_out = data_aus)
+step1_time <- Sys.time()
 
-# Calculating intermediate values for calculating measures
-data_calc <- data_store %>%
-  group_by(run, steps, loc) %>%
-  mutate(total = sum(group_total),
-         pi_m = group_total/total,
-         pi_jm = loc_grp_total/loc_total,
-         pi_j = loc_grp_total/group_total,
-         i = sum(pi_m*(1-pi_m)),
-         e = sum(pi_m*log(1/pi_m)),
-         pi_jm_log = ifelse(pi_jm == 0, 0.001, pi_jm),
-         other_types_total = total-group_total,
-         other_types_loc = loc_total - loc_grp_total,
-         pi_other = other_types_total/total,
-         pi_other_j = other_types_loc/loc_total,
-         pi_other_j_log = ifelse(pi_other_j == 0, 0.001, pi_other_j),
-         e_grp = (pi_m*log(1/pi_m)) + (pi_other*log(1/pi_other)),
-         share_happy = happy/total,
-         share_seg = seg_agents/total) %>%
-  ungroup()
+# Calculating aggregate ratios
+us_agg_ratios <- agg_ratios(us_store)
+uk_agg_ratios <- agg_ratios(uk_store)
+aus_agg_ratios <- agg_ratios(aus_store)
+step2_time <- Sys.time()
 
-# Calculating seggregation measures. Names correspond to the numbering in Notebook files
-data_ratios <- data_calc %>%
-  group_by(run, steps) %>%
-  summarize(one = 0.5*sum((loc_total/(total*i))*(abs(pi_jm - pi_m))),
-            two = sum((loc_total/(total*e))*pi_jm*log(pi_jm_log/pi_m)),
-            four = sum((loc_total/total)* ((pi_jm - pi_m)^2)/((params$types - 1)*pi_m)),
-            five = sum((loc_total/total)*(pi_jm-pi_m)^2),
+# Calculating group ratios
+us_grp_ratios <- grp_ratios(us_store)
+uk_grp_ratios <- grp_ratios(uk_store)
+aus_grp_ratios <- grp_ratios(aus_store)
+step3_time <- Sys.time()
+
+# Combining country cases
+all_agg_ratios <- bind_rows(list(us_agg_ratios, uk_agg_ratios, aus_agg_ratios))
+all_grp_ratios <- bind_rows(list(us_grp_ratios, uk_grp_ratios, aus_grp_ratios))
+
+# Plotting aggregate measures over steps and contrasting country cases
+all_agg_ratios_plot <- all_agg_ratios %>%
+  group_by(cnt, steps) %>%
+  summarize(two = mean(two),
             share_happy = mean(share_happy),
-            share_seg = mean(share_seg))
+            share_seg = mean(share_seg)) %>%
+  gather(key = "Ratio", value = "Value", two, share_happy, share_seg)
 
-data_ratios2 <- data_calc %>%
-  group_by(run, type, steps) %>%
-  summarize(three = sum(pi_j * pi_jm),
-            another_one = sum((loc_total/(total*e_grp))*pi_jm*log(pi_jm_log/pi_m) + (loc_total/(total*e_grp))*pi_other_j*log(pi_other_j_log/pi_other)))
+all_ratio_plotter_agg <- ggplot(all_agg_ratios_plot, aes(x = steps, y = Value, color = cnt)) + 
+  geom_line(size = 1) + theme_bw() + theme(legend.position = "bottom") + ggtitle("Ratios over simulation") + facet_wrap(~Ratio)
 
-# Segregation measures over runs in the simulation
-ratio_plotter <- ggplot(gather(data_ratios, key = "Ratio", value = "Value", one:five), aes(x = steps, y = Value, color = Ratio)) + 
-  geom_line(size = 1) + theme_bw() + theme(legend.position = "bottom") + ggtitle("Ratios over simulation") + facet_wrap(~run)
+pdf("Plots/all_agg_ratios.pdf")
+all_ratio_plotter_agg
+dev.off()
 
-ratio_plotter
+# Plotting group specific measures over steps and contrasting segregation across different types
+all_grp_ratios_plot <- all_grp_ratios %>%
+  group_by(cnt, steps, type) %>%
+  summarize(another_one = mean(another_one))
 
-ratio_plotter2 <- ggplot(data_ratios2, aes(x = steps, y = another_one, color = as.factor(type))) + 
-  geom_line(size = 1) + theme_bw() + theme(legend.position = "bottom") + ggtitle("Ratios over simulation") + facet_wrap(~run)
+all_ratio_plotter_grp <- ggplot(all_grp_ratios_plot, aes(x = steps, y = another_one, color = cnt)) + 
+  geom_line(size = 1) + theme_bw() + theme(legend.position = "bottom") + ggtitle("Ratios over simulation") + facet_wrap(~type)
 
-ratio_plotter2
+pdf("Plots/all_grp_ratios.pdf")
+all_ratio_plotter_grp
+dev.off()
 
-ratio_plotter3 <-ggplot(gather(data_ratios, key = "Ratio", value = "Value", share_happy:share_seg), aes(x = steps, y = Value, color = Ratio)) + 
-  geom_line(size = 1) + theme_bw() + theme(legend.position = "bottom") + ggtitle("Ratios over simulation") + facet_wrap(~run)
-
-ratio_plotter3
-
-# Different kind of plot:
-plot_data <- data_ratios %>%
+# Different kind of plot (Replication on the original one in Mesa example):
+plot_data <- all_agg_ratios %>%
   ungroup() %>%
-  group_by(run) %>%
+  group_by(run, cnt) %>%
   filter(steps == 99)
 
-plot_data$id <- 1
-
-
-point_plot <- ggplot(plot_data, aes(x = as.factor(id), y = share_happy)) + geom_point(alpha = 0.6) + theme_bw() + 
-  ggtitle("Replication of graph in original notes") + scale_y_continuous(limits = c(0, 1))
+point_plot <- ggplot(plot_data, aes(x = cnt, y = share_happy, color = cnt)) + geom_point(alpha = 0.6) + theme_bw() + 
+  ggtitle("Replication of graph in original notes") + scale_y_continuous(limits = c(0, 1)) + xlab("Country")
 
 pdf("Plots/test_plot.pdf", width = 7, height = 5)
 point_plot
 dev.off()
 
-# Tables
-table_example_agg <- data_ratios %>%
+# Table example
+table_example_agg <- all_agg_ratios %>%
   ungroup() %>%
   filter(steps == 24 | steps == 49| steps == 74 | steps == 99) %>%
-  group_by(steps) %>%
+  group_by(steps, cnt) %>%
   summarize(mean_happy = round(mean(share_happy), 4),
             mean_seg = round(mean(share_seg),4 )) %>%
+  ungroup() %>%
   mutate(steps = sprintf("%1.f", steps +1),
          mean_happy = sprintf("%1.2f%%", 100*mean_happy),
          mean_seg = sprintf("%1.2f%%", 100*mean_seg))
 
-colnames(table_example_agg) <- c("Steps", "Happy", "Segregated")
+colnames(table_example_agg) <- c("Steps", "Country", "Happy", "Segregated")
 
 print(xtable(table_example_agg, type = "latex"), include.rownames = F, file = "Tables/test_table.tex")
--- data_collect.py
+++ data_collect.py
@@ -6,23 +6,47 @@ import pandas as pd
 import numpy as np
 
 random.seed(1234)
-# Defining all models
-# model_us = SchellingModel_US(33, 33, 0.7, 0.33, 0.16, 0.17, 5, 1, 0.5)
-# model_uk = SchellingModel_UK(33, 33, 0.7, 0.33, 0.16, 0.17, 5, 1, 0.5)
-# model_aus = SchellingModel_AUS(33, 33, 0.7, 0.33, 0.16, 0.17, 5, 1, 0.5)
 
 # Running multiple times and storing as a csv file
-for i in range(10):
-    model_us = SchellingModel_US(33, 33, 0.7, 0.33, 0.16, 0.17, 5, 1, 0.5)  # Re-defining the model
+for i in range(20):
+    # Defining all models
+    model_us = SchellingModel_US(33, 33, 0.7, 0.33, 0.16, 0.17, 5, 1, 0.5)
+    model_uk = SchellingModel_UK(33, 33, 0.7, 0.33, 0.16, 0.17, 5, 1, 0.5)
+    model_aus = SchellingModel_AUS(33, 33, 0.7, 0.33, 0.16, 0.17, 5, 1, 0.5)
+
     while model_us.running and model_us.schedule.steps < 100:
         model_us.step()
     model_us_data = model_us.datacollector.get_model_vars_dataframe()  # Storing data of a single run
+
+    while model_uk.running and model_uk.schedule.steps < 100:
+        model_uk.step()
+    model_uk_data = model_uk.datacollector.get_model_vars_dataframe()  # Storing data of a single run
+
+    while model_aus.running and model_aus.schedule.steps < 100:
+        model_aus.step()
+    model_aus_data = model_aus.datacollector.get_model_vars_dataframe()  # Storing data of a single run
+
     ider = i+1  # Run identifer
     print(ider)
+
     model_us_data['run'] = np.repeat(ider, len(model_us_data))  # Adding run identifier
+    model_us_data['cnt'] = np.repeat("US", len(model_us_data))  # Adding country identifier
+
+    model_uk_data['run'] = np.repeat(ider, len(model_uk_data))  # Adding run identifier
+    model_uk_data['cnt'] = np.repeat("UK", len(model_uk_data))  # Adding country identifier
+
+    model_aus_data['run'] = np.repeat(ider, len(model_aus_data))  # Adding run identifier
+    model_aus_data['cnt'] = np.repeat("AUS", len(model_aus_data))  # Adding country identifier
+
     if i == 0:
         model_us_all = model_us_data  # If it is first run assign to the dataframe
+        model_uk_all = model_uk_data
+        model_aus_all = model_aus_data
     else:
         model_us_all = model_us_all.append(model_us_data)  # Otherwise row bind
+        model_uk_all = model_uk_all.append(model_uk_data)
+        model_aus_all = model_aus_all.append(model_aus_data)
 
 model_us_all.to_csv('data/out_us.csv')  # Save the file
+model_uk_all.to_csv('data/out_uk.csv')  # Save the file
+model_aus_all.to_csv('data/out_aus.csv')  # Save the file
